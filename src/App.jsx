18fd0d95631423d6c492ae1077587d10af6be073
birnares <birnares08@gmail.com>
2024-02-15 14:30:13 +0500

    Добавил antd layout + разбил на компоненты

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { Layout } from 'antd'
+import React from 'react'
+import AppHeader from './components/layout/AppHeader'
+import AppSider from './components/layout/AppSider'
+
+const contentStyle = {
+	textAlign: 'center',
+	minHeight: 'calc(100vh - 60px)',
+	color: '#fff',
+	backgroundColor: '#001529',
+}
+
 export default function App() {
-  return <h1>React Crypto App</h1>
+	return (
+		<>
+			<Layout>
+				<AppHeader />
+				<Layout>
+					<AppSider />
+					<Layout.Content style={contentStyle}>Content</Layout.Content>
+				</Layout>
+			</Layout>
+		</>
+	)
 }
